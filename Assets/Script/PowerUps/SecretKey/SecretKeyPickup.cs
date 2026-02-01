12b81ea02f993b7f19c2a1e33a2e813b11ac5a2b
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2026-02-01 11:50:28 -0500

    Ui santi

--- Assets/Script/PowerUps/SecretKey/SecretKeyPickup.cs
+++ Assets/Script/PowerUps/SecretKey/SecretKeyPickup.cs
@@ -6,7 +6,7 @@ namespace Script.PowerUps.SecretKey
     {
         [SerializeField] private GameCapabilityState capabilityState;
 
-        private void OnCollisionEnter2D(Collision2D other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.gameObject.CompareTag("Player")) return;
             
