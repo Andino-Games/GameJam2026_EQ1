5cd00863e5cf2a3b88de44d699cee44c5a51cb25
Camilo Rojas <cm.andres022@hotmail.com>
2026-02-20 22:33:52 -0500

    feature: Add Text animator integration

---
+++ Assets/Tools/Pixel Crushers/Common/Third Party Support/Text Animator Support/TextAnimatorEndKeywordByCharacter.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Pixel Crushers. All rights reserved.
+
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    /// <summary>
+    /// Add this to your dialogue UI if you want the sequencer {{end}} keyword to
+    /// account for Text Animator typing time and typewriter types by character.
+    /// </summary>
+    public class TextAnimatorEndKeywordByCharacter : MonoBehaviour
+    {
+        
+        public float waitForNormalChars = .03f;
+        public float waitLong = .6f;
+        public float waitMiddle = .2f;
+
+        protected virtual void Awake()
+        {
+            ConversationView.overrideGetDefaultSubtitleDuration = GetTextAnimatorSubtitleDuration;
+        }
+
+        protected virtual float GetTextAnimatorSubtitleDuration(string text)
+        {
+            // Remove markup tags:
+            var cleanText = text.Contains('<')
+                ? Regex.Replace(text, @"<[^> ]+>", string.Empty)
+                : text;
+
+            int numMiddle = 0;
+            int numLong = 0;
+            for (int i = 0; i < cleanText.Length; i++)
+            {
+                char c = cleanText[i];
+                var isMiddleLengthChar = c == ';' || c == ':' || c == ')' || c == '-' || c == ',';
+                var isLongChar = c == '!' || c == '?' || c == '.';
+                if (isMiddleLengthChar) numMiddle++;
+                else if (isLongChar) numLong++;
+            }
+            int numNormal = cleanText.Length - (numMiddle + numLong);
+            return (waitForNormalChars * numNormal) + 
+                (waitMiddle * numMiddle) + 
+                (waitLong * numLong);
+        }
+
+    }
+}
---
+++ Assets/Tools/Pixel Crushers/Common/Third Party Support/Text Animator Support/TextAnimatorEndKeywordByWord.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Pixel Crushers. All rights reserved.
+
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    /// <summary>
+    /// Add this to your dialogue UI if you want the sequencer {{end}} keyword to
+    /// account for Text Animator typing time and typewriter types by word.
+    /// </summary>
+    public class TextAnimatorEndKeywordByWord : MonoBehaviour
+    {
+
+        public float waitForNormalWord = 0.3f;
+        public float waitForWordWithPunctuation = 0.5f;
+
+        protected virtual void Awake()
+        {
+            ConversationView.overrideGetDefaultSubtitleDuration = GetTextAnimatorSubtitleDuration;
+        }
+
+        protected virtual float GetTextAnimatorSubtitleDuration(string text)
+        {
+            // Remove markup tags:
+            var cleanText = text.Contains('<')
+                ? Regex.Replace(text, @"<[^> ]+>", string.Empty)
+                : text;
+
+            int numNormalWords = 0;
+            int numPunctuationWords = 0;
+            char prevChar = ' ';
+            for (int i = 0; i < cleanText.Length; i++)
+            {
+                char c = cleanText[i];
+                var isEndOfNormalWord = c == ' ' && prevChar != ' ';
+                var isEndOfPunctuationWord = char.IsPunctuation(c) && char.IsLetterOrDigit(prevChar);
+                if (isEndOfNormalWord) numNormalWords++;
+                else if (isEndOfPunctuationWord) numPunctuationWords++;
+                prevChar = c;
+            }
+            return
+                (waitForNormalWord * numNormalWords) +
+                (waitForWordWithPunctuation * numPunctuationWords);
+        }
+
+    }
+}
